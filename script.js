d6ce143e79f22c700a1e3525d1fc75f854ef6dac
KamilPiech <718133+KamilPiech@users.noreply.github.com>
2024-10-21 12:28:33 +0200

    Add basic game logic

--- script.js
+++ script.js
@@ -10,16 +10,72 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
-    return prompt("What's your choice?").toLowerCase();
+    let humanInput = prompt("What's your choice?").toLowerCase();
+    
+    if (humanInput == "rock" || humanInput == "paper" || humanInput == "scissors") {
+        return humanInput;
+    } else {
+        console.log("Wrong input, try again!");
+        return getHumanChoice();
+    }
 }
 
-console.log(getHumanChoice());
 
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        return console.log("Remis");
+    }
+
+    if (humanChoice == "rock") {
+        if (computerChoice == "paper") {
+            console.log("Computer Won");
+            return computerScore +=1;    
+        } else {
+            console.log("Player Won");           
+            return humanScore += 1;
+        }
+    }
+
+    if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            console.log("Player Won");
+            return humanScore +=1;
+        } else {
+            console.log("Computer Won");
+            return computerScore += 1;
+        }
+    }
+
+    if (humanChoice == "scissors") {
+        if (computerChoice == "rock") {
+            console.log("Computer Won");
+            return computerScore += 1;
+        } else {
+            console.log("Player Won");
+            return humanScore += 1;
+        }
+    }
+}
+
+const computerSelection = getComputerChoice();
+let humanSelection = getHumanChoice();
+
+console.log(humanSelection);
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+console.log(computerSelection);
+
+
+playRound(humanSelection, computerSelection);
+
+console.log("Player: " + humanScore);
+console.log("Computer: " + computerScore);
+
+
 
 
 
